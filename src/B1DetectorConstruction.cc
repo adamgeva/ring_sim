bb2e193d91d6a71114e6c43992e6237b83a4c925
Adam Geva <adamgeva@vislbatch2.technion.ac.il>
2017-05-18 16:40:22 +0300

    changed geometry to poing spread function

--- src/B1DetectorConstruction.cc
+++ src/B1DetectorConstruction.cc
@@ -59,19 +59,19 @@ G4VPhysicalVolume* B1DetectorConstruction::Construct()
 	// Water phantom
 	//todo: make generic and not hard coded
 	G4Material* waterMat = nist->FindOrBuildMaterial("G4_WATER");
-	G4Tubs* waterPhantomS = new G4Tubs("water_phantom",0,10*cm,50*cm,0,2*M_PI);
+	G4Tubs* waterPhantomS = new G4Tubs("water_phantom",0,0.5*cm,0.5*cm,0,2*M_PI);
 	G4LogicalVolume* waterPhantomLV = new G4LogicalVolume(waterPhantomS,waterMat,"water_phantom");
 	G4RotationMatrix* rot = new G4RotationMatrix();
 	rot->rotateX(-M_PI/2);
 	new G4PVPlacement(rot,G4ThreeVector(),waterPhantomLV,"water_phantom",worldLV,false,0,checkOverlaps);
 
-	//bone
-	//todo: make generic and not hard coded
-	G4Material* boneMat = nist->FindOrBuildMaterial("G4_BONE_COMPACT_ICRU");
-	G4ThreeVector posBone = G4ThreeVector(0, 0, 0);
-	G4Tubs* boneS = new G4Tubs("bone",0, 2*cm, 50*cm,0,2*M_PI);
-	G4LogicalVolume* boneLV = new G4LogicalVolume(boneS,boneMat,"bone");
-	new G4PVPlacement(0,posBone,boneLV,"bone",waterPhantomLV,false,0,checkOverlaps);
+//	//bone
+//	//todo: make generic and not hard coded
+//	G4Material* boneMat = nist->FindOrBuildMaterial("G4_BONE_COMPACT_ICRU");
+//	G4ThreeVector posBone = G4ThreeVector(0, 0, 0);
+//	G4Tubs* boneS = new G4Tubs("bone",0, 2*cm, 50*cm,0,2*M_PI);
+//	G4LogicalVolume* boneLV = new G4LogicalVolume(boneS,boneMat,"bone");
+//	new G4PVPlacement(0,posBone,boneLV,"bone",waterPhantomLV,false,0,checkOverlaps);
 
 	// detector - specs
 	G4Material* detectorMat = nist->FindOrBuildMaterial("G4_CESIUM_IODIDE");
@@ -120,10 +120,10 @@ G4VPhysicalVolume* B1DetectorConstruction::Construct()
 	detectorLV->SetVisAttributes(visAttributes);
 	fVisAttributes.push_back(visAttributes);
 
-	//bone
-	visAttributes = new G4VisAttributes(G4Colour(0.8888,0.0,0.0));
-	boneLV->SetVisAttributes(visAttributes);
-	fVisAttributes.push_back(visAttributes);
+//	//bone
+//	visAttributes = new G4VisAttributes(G4Colour(0.8888,0.0,0.0));
+//	boneLV->SetVisAttributes(visAttributes);
+//	fVisAttributes.push_back(visAttributes);
 
 	//water phantom
 	visAttributes = new G4VisAttributes(G4Colour(0.0,0.0,1.0));
